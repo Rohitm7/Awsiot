7321912a4e40895da1234118f03ce93cb1921825
Rohit <mahajanr124@gmail.com>
2022-12-23 10:20:24 +0530

    Delete File Data Project

---
+++ delete_files/src/main/java/delete_files/Delete_File.java
@@ -0,0 +1,24 @@
+package delete_files;
+
+import java.io.File;
+
+public class Delete_File {
+	public static void Delete_screnshots() {
+		String path = "/Users/aneesh/Desktop/Screenshots";
+		File file = new File(path);
+		File[] files = file.listFiles();
+		for (File f : files) {
+			if (f.isFile() && f.exists()) {
+				f.delete();
+				System.out.println("successfully deleted");
+			} else {
+				System.out.println("cant delete a file due to open or error");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Delete_screnshots();
+	}
+
+}
