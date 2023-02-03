68d9df350851900dba5dc746ee36f5e00e03ef6b
Rohit Mahajan <mahajanr124@gmail.com>
2023-02-03 12:35:28 +0530

    s

--- Api_hourly_request/src/main/java/Config_Utility/config_read.java
+++ Api_hourly_request/src/main/java/Config_Utility/config_read.java
@@ -8,8 +8,7 @@ import java.util.Properties;
 public class config_read {
 
 	public static String read_configvalue(String Key) throws Exception {
-		Properties prop = new Properties();
-		
+		Properties prop = new Properties();	
 		String Filename="config.properties";
         String Working_Directory=System.getProperty("user.dir");
         System.out.println("Working_Directory :"+Working_Directory); 
@@ -17,8 +16,7 @@ public class config_read {
         String FilePath=Working_Directory +File.separator+"src"+File.separator+"main"+
         File.separator+"resources"+File.separator+Filename;
         
-        System.out.println("File path by using file separator :"+FilePath);
-		
+        System.out.println("File path by using file separator :"+FilePath);		
 		FileInputStream ip = new FileInputStream(FilePath);
 		prop.load(ip);
 		String value = prop.getProperty(Key);
