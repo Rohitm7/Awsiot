4b7f75dff217c7f31d31fbf270a4fe5ecb98c0ae
Rohit Mahajan <mahajanr124@gmail.com>
2023-03-16 18:04:50 +0530

    test

--- Infosys_Dash_ping/src/main/java/Mail_Utility/Sending_Mail_html.java
+++ Infosys_Dash_ping/src/main/java/Mail_Utility/Sending_Mail_html.java
@@ -79,8 +79,8 @@ public class Sending_Mail_html {
 
 		String password = config_read.read_configvalue("apppassword");
 		
-		String encryptedpassword = Base64.getEncoder().encodeToString(password.getBytes());
-		System.out.println("Encrypted password: " + encryptedpassword);
+		//String encryptedpassword = Base64.getEncoder().encodeToString(password.getBytes());
+		//System.out.println("Encrypted password: " + encryptedpassword);
 
 		// Message info
 		String mailTo = config_read.read_configvalue("mailTo");
@@ -128,7 +128,7 @@ public class Sending_Mail_html {
 		Calendar now = Calendar.getInstance();
 
 		try {
-			sendEmailWithAttachments(host, port, mailFrom, encryptedpassword, mailTo, subject, message);
+			sendEmailWithAttachments(host, port, mailFrom, password, mailTo, subject, message);
 			// System.out.println("Email sent Succesfully..");
 		} catch (Exception ex) {
 			ex.printStackTrace();
