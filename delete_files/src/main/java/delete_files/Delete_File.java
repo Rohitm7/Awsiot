0b4952ce0bbcdf1763f8e20fd62231bf38486dc4
Rohit Mahajan <mahajanr124@gmail.com>
2023-04-19 10:05:11 +0530

    path

--- delete_files/src/main/java/delete_files/Delete_File.java
+++ delete_files/src/main/java/delete_files/Delete_File.java
@@ -7,15 +7,12 @@ public class Delete_File {
 		String path = "/Users/testingwimate/Desktop/Automation_Screenshots/";
 		File file = new File(path);
 		File[] files = file.listFiles();
-		for (int i = 0; i < files.length; i++) {
-			System.out.println(file.getName());
-			for (File f : files) {
-				if (f.isFile() && f.exists()) {
-					// f.delete();
-					System.out.println("successfully deleted");
-				} else {
-					System.out.println("cant delete a file due to open or error");
-				}
+		for (File f : files) {
+			if (f.isFile() && f.exists()) {
+				f.delete();
+				System.out.println("successfully deleted");
+			} else {
+				System.out.println("cant delete a file due to open or error");
 			}
 		}
 	}
