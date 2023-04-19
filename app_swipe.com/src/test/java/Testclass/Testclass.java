3953ce305f846dd44a2ed58a3647b5cf76647b8d
Rohit Mahajan <mahajanr124@gmail.com>
2023-04-19 13:03:36 +0530

    test

--- app_swipe.com/src/test/java/Testclass/Testclass.java
+++ app_swipe.com/src/test/java/Testclass/Testclass.java
@@ -1,9 +1,16 @@
 package Testclass;
 
 import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
+
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class Testclass {
 	static AndroidDriver driver;
@@ -18,19 +25,33 @@ public class Testclass {
 	//	cap.setCapability("appPackage", "com.sec.android.gallery3d");
 	//	cap.setCapability("appActivity", "com.samsung.android.gallary.app.activity.GalleryActivity");
 		
-	//	cap.setCapability("appPackage", "io.ionic.starter.untangled_ams_task");
-	//	cap.setCapability("appActivity", "io.ionic.starter.untangled_ams_task.MainActivity");
-		
-		cap.setCapability("appPackage", "com.android.chrome");
-		cap.setCapability("appActivity", "com.google.android.gms.common.api.GoogleApiActivity");
-
+		cap.setCapability("appPackage", "io.ionic.starter.untangled_ams_task");
+		cap.setCapability("appActivity", "io.ionic.starter.untangled_ams_task.MainActivity");
+			
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
 		driver = new AndroidDriver(url, cap);
-
 		System.out.println("Application Launched");
 		
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("(//*[@index='0'])[21]")).sendKeys("ams_automation");
+		driver.findElement(By.xpath("(//*[@index='0'])[25]")).sendKeys("Ams_automati0n");
+		driver.findElement(By.xpath("//android.widget.Button[@text='SIGN IN']")).click();
 
 		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Assigned Tickets']")).click();
+
+		Thread.sleep(2000);
+		
+		TouchAction action = new TouchAction(driver);
+		action.press(PointOption.point(500, 1500))
+		      .waitAction(WaitOptions.waitOptions(Duration.ofMillis(1000)))
+		      .moveTo(PointOption.point(500, 1000))
+		      .release()
+		      .perform();
+
+		
 	}
 
 }
