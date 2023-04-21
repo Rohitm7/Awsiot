d0c99973db4f738d8f075f1ea3dfe7b9b68fc950
Rohit Mahajan <mahajanr124@gmail.com>
2023-04-21 10:52:51 +0530

    form parameter added...

--- app_swipe.com/src/test/java/Testclass/Testclass.java
+++ app_swipe.com/src/test/java/Testclass/Testclass.java
@@ -17,110 +17,93 @@ import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 
 public class Testclass {
-	static AndroidDriver  driver;
-	
+	static AndroidDriver driver;
+
 	@Test
-	public void testcases() throws Exception{
+	public void testcases() throws Exception {
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability("deviceName", "Samsung Testing Phone");
 		cap.setCapability("udid", "10.0.1.130:5555");
 		cap.setCapability("platformName", "Android");
 		cap.setCapability("platformVersion", "13");
 
-	//	cap.setCapability("appPackage", "com.sec.android.gallery3d");
-	//	cap.setCapability("appActivity", "com.samsung.android.gallary.app.activity.GalleryActivity");
-		
+		// cap.setCapability("appPackage", "com.sec.android.gallery3d");
+		// cap.setCapability("appActivity",
+		// "com.samsung.android.gallary.app.activity.GalleryActivity");
+
 		cap.setCapability("appPackage", "io.ionic.starter.untangled_ams_task");
 		cap.setCapability("appActivity", "io.ionic.starter.untangled_ams_task.MainActivity");
-			
+
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
 		driver = new AndroidDriver(url, cap);
 		System.out.println("Application Launched");
-		
+
 		Thread.sleep(2000);
-		
+
 		driver.findElement(By.xpath("(//*[@index='0'])[21]")).sendKeys("ams_automation");
 		driver.findElement(By.xpath("(//*[@index='0'])[25]")).sendKeys("Ams_automati0n");
 		driver.findElement(By.xpath("//android.widget.Button[@text='SIGN IN']")).click();
 
 		Thread.sleep(3000);
-		
+
 		driver.findElement(By.xpath("(//android.view.View[@index='4'])[2]")).click();
 
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//android.widget.Button[@text='SCAN']")).click();
 		Thread.sleep(1500);
-		
+
 		driver.findElement(By.xpath("//android.widget.Button[@text='While using the app']")).click();
 		Thread.sleep(500);
 
+		// Form Type = Test_Parameter30ID
+		// Id Webelement
+		driver.findElement(By.xpath("(//android.widget.EditText[@index='0'])[1]")).sendKeys("Test_R_ID_01");
+		// Description
+		driver.findElement(By.xpath("(//android.widget.EditText[@index='0'])[2]")).sendKeys("Test_R_Des_01");
+		// Parameter_2 Text
+		driver.findElement(By.xpath("(//android.widget.EditText[@index='0'])[4]")).sendKeys("R_Text_01");
+		// Parameter_3 Number
+		driver.findElement(By.xpath("(//android.widget.EditText[@index='0'])[5]")).sendKeys(String.valueOf(7));
+		// Parameter_4 Text
+		driver.findElement(By.xpath("(//android.widget.EditText[@index='0'])[5]")).sendKeys("R_Text_02");
+		Thread.sleep(1500);
 
-		
-/*
- * Touch Action class is deprecated so in upcoming version it will be removed.
- */
-				
-//		TouchAction action = new TouchAction(driver);
-//		action.press(PointOption.point(500, 1500))
-//		      .waitAction(WaitOptions.waitOptions(Duration.ofMillis(1000)))
-//		      .moveTo(PointOption.point(500, 1000))
-//		      .release()
-//		      .perform();
-
-/*
- * For this one Java Client API should 7.6.0 but in that also throwing some error.
- */
-		
-		// Define the starting and ending coordinates of the swipe gesture
-		
-//		MobileElement element = driver.findElement(By.xpath("//android.widget.TextView[text()='ID: 1681445918439']"));
-//
-//		// Get the location and size of the element
-//		Point location = element.getLocation();
-//		Dimension size = element.getSize();
-//
-//		// Define the start and end point coordinates for the swipe gesture
-//		int startX = location.getX() + (int) (0.5 * size.getWidth());
-//		int startY = location.getY() + (int) (0.8 * size.getHeight());
-//		int endX = location.getX() + (int) (0.5 * size.getWidth());
-//		int endY = location.getY() + (int) (0.2 * size.getHeight());
-//		
-//		System.out.println("startX :"+startX);
-//		System.out.println("startX :"+startY);
-//		System.out.println("startX :"+endX);
-//		System.out.println("startX :"+endY);
-
-		
 		/*
-		 * This will support in new version w3c scroll 
+		 * Touch Action class is deprecated so in upcoming version it will be removed.
 		 */
-		//Using 500,1500 pointer will go touch to the middle of the page.
-		Point startPoint = new Point(500, 1500);
-		//Using 0,0  100% page is scrooling upto 7 Assigned Ticeket is scrolling...
-		//Using -500,-1500 300% page is scrolling upto 21 Assigned Ticket is scrolling.
-		Point endPoint = new Point(0, 0);
 
+		TouchAction action = new TouchAction(driver);
+		action.press(PointOption.point(500, 1500))
+		.waitAction(WaitOptions.waitOptions(Duration.ofMillis(1000)))
+				.moveTo(PointOption.point(500, 1000))
+				.release()
+				.perform();
+
+		/*
+		 * This will support in new version w3c scroll
+		 */
+		// Using 500,1500 pointer will go touch to the middle of the page.
+		Point startPoint = new Point(500, 1500);
+		// Using 0,0 100% page is scrooling upto 7 Assigned Ticeket is scrolling...
+		// Using -500,-1500 300% page is scrolling upto 21 Assigned Ticket is scrolling.
+		Point endPoint = new Point(0, 0);
 		// Create a new PointerInput object
 		PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
-
 		// Create a new Sequence object
 		Sequence swipe = new Sequence(finger, 2);
-
 		// Add a pointer move action to the starting point
-		swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startPoint.x, startPoint.y));
-
+		swipe.addAction(
+				finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startPoint.x, startPoint.y));
 		// Add a pointer down action to simulate touching the screen
 		swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-
 		// Add a pointer move action to the ending point
-		swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), endPoint.x, endPoint.y));
-
+		swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), endPoint.x,
+				endPoint.y));
 		// Add a pointer up action to simulate releasing the screen
 		swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-
 		// Perform the swipe gesture
 		driver.perform(Arrays.asList(swipe));
-		
+
 	}
 
 }
